400cc27fdfbd2e6cad201e327a087adf96ae5628
Ilya Surmay <ilya.surmay@gmail.com>
2019-02-22 13:04:55 +0200

    Updates

--- bot.js
+++ bot.js
@@ -10,32 +10,33 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 Object.defineProperty(exports, "__esModule", { value: true });
 const botbuilder_1 = require("botbuilder");
 const IS_AUTHORIZED = 'isAuthorizedProperty';
+const IS_WELCOME_MESSAGE_SENT = 'isWelcomeMessageSent';
 class SkypeBot {
     constructor(userState) {
         this.userState = userState;
         this.isAuthorizedProperty = userState.createProperty(IS_AUTHORIZED);
+        this.isWelcomeMessageSent = userState.createProperty(IS_WELCOME_MESSAGE_SENT);
     }
     onTurn(turnContext) {
         return __awaiter(this, void 0, void 0, function* () {
+            console.log('turnContext.activity.type', turnContext.activity.type);
             if (turnContext.activity.type === botbuilder_1.ActivityTypes.Message) {
-                yield this.userState.saveChanges(turnContext);
             }
             else if (turnContext.activity.type === botbuilder_1.ActivityTypes.ConversationUpdate || turnContext.activity.type === botbuilder_1.ActivityTypes.ContactRelationUpdate) {
-                yield this.sendWelcomeMessage(turnContext);
+                const isWelcomeMessageSent = yield this.isWelcomeMessageSent.get(turnContext, false);
+                if (!isWelcomeMessageSent) {
+                    yield this.sendWelcomeMessage(turnContext);
+                    yield this.isWelcomeMessageSent.set(turnContext, true);
+                }
+                yield this.userState.saveChanges(turnContext);
             }
         });
     }
     sendWelcomeMessage(turnContext) {
         return __awaiter(this, void 0, void 0, function* () {
-            if (turnContext.activity.membersAdded && turnContext.activity.membersAdded.length) {
-                for (const i in turnContext.activity.membersAdded) {
-                    if (turnContext.activity.membersAdded[i].id !== turnContext.activity.recipient.id) {
-                        yield turnContext.sendActivity(`Welcome! 
-          Please enter your email and a verification code in the following format: "your@email.com XXXXXXXX", where XXXXXXXX is your verification code. 
-          You can get you verification code in your user profile in Renaizant.`);
-                    }
-                }
-            }
+            yield turnContext.sendActivity(`Welcome! 
+    Please enter your email and a verification code in the following format: "your@email.com XXXXXXXX", where XXXXXXXX is your verification code. 
+    You can get you verification code in your user profile in Renaizant.`);
         });
     }
 }
